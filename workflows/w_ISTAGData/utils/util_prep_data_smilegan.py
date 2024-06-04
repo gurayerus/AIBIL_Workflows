da7f98d7bc026fd03b611e21e144996ea70904b2
gurayerus <gurayerus@gmail.com>
2024-06-04 12:44:15 -0400

    Edit

--- workflows/w_ISTAGData/utils/util_prep_data_smilegan.py
+++ workflows/w_ISTAGData/utils/util_prep_data_smilegan.py
@@ -3,47 +3,21 @@ import argparse
 import json
 import sys
 
-def select_sample(in_sample, in_csv, in_rois, in_vars, out_csv):
+def select_data_smilegan(in_csv, out_csv):
 
     """
-    Select data based on list of samples and list of variables
-     - in_sample: List with MRIDs of samples
-     - in_vars: list of variables
-     - in_rois: list of additional ROI variables (with columns Index,Name)
-                the column Name is used to select additional ROI variables
-     - in_var_names: list of var names to rename additional variables
+    Create smilegan input data
     """
     
-    # Read input files
+    # Read input file
     df = pd.read_csv(in_csv)
-    dfs = pd.read_csv(in_sample)
-    dfr = pd.read_csv(in_rois)
 
-    # Convert columns of dataframe to str (to handle numeric ROI indices)
-    df.columns = df.columns.astype(str)
+    # Get demog columns
+    df = pd.read_csv(in_csv)
+    
+    # Get roi columns
+    df = pd.read_csv(in_csv)
 
-    # Get variable lists (input var list + rois)
-    in_vars = in_vars.split(',')
-    roi_vars = dfr.Name.astype(str).tolist()
-    
-    # Remove duplicate vars (in case a variable is both in roi list and input var list)
-    in_vars = [x for x in in_vars if x not in roi_vars]
-
-    # Make a list of selected variables
-    sel_vars = in_vars + roi_vars 
-    in_key = sel_vars[0]
-    
-    # Select variables
-    df_out = df[sel_vars]
-
-    # Merge with selected sample
-    #  Duplicate columns are discarded
-    df_out = dfs.merge(df_out, on = in_key, suffixes = ['', '_tmpremovedupl'])
-    df_out = df_out[df_out.columns[df_out.columns.str.contains('_tmpremovedupl')==False]]
-    
-    # Drop duplicate columns in MUSE ROIs (ROIs repeated in single and composite ROI list)
-    df_out = df_out.loc[:, df_out.columns.duplicated()==False]    
-    
     # Write out file
     df_out.to_csv(out_csv, index=False)
 
