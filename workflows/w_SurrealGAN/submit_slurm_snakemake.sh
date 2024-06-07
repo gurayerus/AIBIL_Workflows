1202f3b0b1c92913271e7396783321b8016ff457
gurayerus <gurayerus@gmail.com>
2024-06-06 20:33:05 -0400

    Edit

--- workflows/w_SurrealGAN/submit_slurm_snakemake.sh
+++ workflows/w_SurrealGAN/submit_slurm_snakemake.sh
@@ -3,11 +3,11 @@
 #SBATCH --mem=32000
 #SBATCH --output=slurm_logs/snakemake-%j.out
 
-source activate /cbica/home/erusg/.conda/envs/snakemake-tutorial
-
-cd /cbica/home/erusg/GitHub/NiChart/NiChart_Workflows/NiChart_Engine/NiChart_MLAnalytics/workflows/w_spare
+source activate /cbica/home/erusg/.conda/envs/surrealgan312
 
 # snakemake --cores=1 --executor slurm --default-resources slurm_account=davatzic --jobs 1 
-snakemake --cores=1 --executor slurm --default-resources slurm_account=davatzic --jobs 10 
+
+snakemake --cores=1 --executor slurm --default-resources slurm_account=davatzic --jobs 1 
+# snakemake --cores=1 --executor slurm --default-resources --jobs 1 
 
 conda deactivate
